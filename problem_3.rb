4eb915e282a8f6081b0ab590016347cde318a611
Edwin Castillo <edgcastillo@gmail.com>
2015-02-05 18:46:26 -0500

    Delete files

--- problem_3.rb
+++
@@ -1,9 +0,0 @@
-def largest_prime_factor(num)
-	prime_factor_numbers = []
-	Prime.each(num) do |prime|
-		prime_factor_numbers.push(prime) if(num / prime != Float)
-	end
-	prime_factor_numbers
-end
-
-p largest_prime_factor(13195)
